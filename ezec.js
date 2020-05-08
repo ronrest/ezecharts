8706635fdd415c73aa23c3e4ba05a8084bf50a8e
ronrest <ronny.coding@gmail.com>
2020-05-08 20:07:20 +1000

    FEAT: add DF class

--- ezec.js
+++ ezec.js
@@ -20,3 +20,42 @@ function getOrThrow(obj, key){
     if (key in obj){ return obj[key]} else {throw "Missing property: " + key};
     // return (key in obj) ? obj[key] : throw "Missing property: " + key;
 }
+
+
+
+// #############################################################################
+// DATAFRAE FUNCTIONS
+// #############################################################################
+class DF{
+    // Initialized in the constructor func
+    constructor(data, schema) {
+        this.data = data;
+        this.schema = schema;
+        this.columns = data[0];
+        // this.shape = TODO
+    }
+
+    colname2idx(colname){
+        var idx = this.columns.indexOf(colname);
+        if (idx == -1){
+            return null;
+        } else {
+            return idx;
+        }
+    }
+
+    colnames2idx(colnames){
+        var indices;
+        colnames.forEach(function(x){
+            indices.push(this.colname2idx(x));
+        });
+    }
+
+    idx2colname(idx){
+        return this.columns[idx]
+    }
+
+    // TODO: slice by column names, column indices, and multiple
+    //       columns at once
+
+}
