4c3a34c0b76fa258e3158429fce2509b81e04b8c
ronrest <ronny.coding@gmail.com>
2020-05-12 14:52:59 +1000

    STYLE: comment out old unused functions

--- ezec.js
+++ ezec.js
@@ -416,48 +416,48 @@ function getOrThrow(obj, key){
 // #############################################################################
 // DATAFRAE FUNCTIONS
 // #############################################################################
-function sliceDF(df, cols){
-    // Given tabular data as a list of lists, with first row
-    // containing column names, and a list of column names to
-    // extract, it returns just those columns from the data
-    var colnames = df[0]; // column names in original data
-    var colname2id = colnames.reduce((total, x, i) => Object.assign(total, {[x]: i}), {});
+// function sliceDF(df, cols){
+//     // Given tabular data as a list of lists, with first row
+//     // containing column names, and a list of column names to
+//     // extract, it returns just those columns from the data
+//     var colnames = df[0]; // column names in original data
+//     var colname2id = colnames.reduce((total, x, i) => Object.assign(total, {[x]: i}), {});
+//
+//     var colIds = cols.map(x => colname2id[x]); // ids of desired cols
+//     return df.map(x => colIds.map(i => x[i]))
+// };
 
-    var colIds = cols.map(x => colname2id[x]); // ids of desired cols
-    return df.map(x => colIds.map(i => x[i]))
-};
-
-function getDFcolumn(df, col, include_header=true){
-    // Given tabular data as a list of lists, with first row
-    // containing column names, column name to extract, it
-    // returns just that row as a flat list
-    // Optionally you can exclude the header label
-    var colnames = df[0]; // column names in original data
-    var colname2id = colnames.reduce((total, x, i) => Object.assign(total, {[x]: i}), {});
-
-    var colId = colname2id[col]; // id of desired col
-    var output = df.map(x => x[colId]);
-    if (include_header){
-        return output;
-    } else {
-        return output.slice(1);
-    }
-};
-
-function getUniqueValsInColumn(df, col, numerical=false){
-    // gets unique values in column, and tries to sort them in order
-    // set numerical=true to sort them properly as numbers
-    // if the values in column are numerical
-    var x = getDFcolumn(df=df, col=col, include_header=false);
-    x = Array.from(new Set(x));
-
-    if (numerical){
-        x.sort(function(a, b){return a - b});
-    }else{
-        x.sort();
-    };
-    return x
-};
+// function getDFcolumn(df, col, include_header=true){
+//     // Given tabular data as a list of lists, with first row
+//     // containing column names, column name to extract, it
+//     // returns just that row as a flat list
+//     // Optionally you can exclude the header label
+//     var colnames = df[0]; // column names in original data
+//     var colname2id = colnames.reduce((total, x, i) => Object.assign(total, {[x]: i}), {});
+//
+//     var colId = colname2id[col]; // id of desired col
+//     var output = df.map(x => x[colId]);
+//     if (include_header){
+//         return output;
+//     } else {
+//         return output.slice(1);
+//     }
+// };
+//
+// function getUniqueValsInColumn(df, col, numerical=false){
+//     // gets unique values in column, and tries to sort them in order
+//     // set numerical=true to sort them properly as numbers
+//     // if the values in column are numerical
+//     var x = getDFcolumn(df=df, col=col, include_header=false);
+//     x = Array.from(new Set(x));
+//
+//     if (numerical){
+//         x.sort(function(a, b){return a - b});
+//     }else{
+//         x.sort();
+//     };
+//     return x
+// };
 
 
 class DF{
